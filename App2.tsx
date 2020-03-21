adb192c82f91b05a29bc787464d0b6f6a885e91b
Kotwic4 <kotwic4@gmail.com>
2020-03-21 17:39:51 +0100

    wip

---
+++ App2.tsx
@@ -0,0 +1,86 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Secret Hitler</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+
+// @import url('https://fonts.googleapis.com/css2?family=Courier+Prime:wght@700&display=swap');
+//
+// * {
+//   box-sizing: border-box;
+// }
+//
+// body {
+//   background-color: #FBB969;
+//   font-family: 'Courier Prime', Courier, monospace;
+//   font-size: 18px;
+//   font-weight: 700;
+//   line-height: 1.4;
+//   color: #434343;
+//   padding: 0 2em;
+// }
+//
+// a:hover {
+//   color: #968975;
+//   cursor: pointer;
+// }
+//
+// a {
+//   color: #434343;
+//   font-weight: 700;
+//   text-decoration: underline;
+//   transition: color 60ms ease;
+// }
+//
+// hr {
+//   background-color: transparent;
+//   background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' height='9' width='849' viewBox='0 0 849 9'%3E%3Cpath fill='%23444444' d='M601,1.8c-2.3,1.2-3.8,1.5-6.6,1.3c-0.4,0-0.9-0.4-1.2-0.8c-0.4-0.6-0.6-1.4-0.5-2.1H489.7 c0.2,0.2,0.5,0.4,0.7,0.7c0.3,0.3,0.2,0.9,0.3,1.4c0.5,1,0.4,1.9-0.5,2.5c-0.9,0.7-2-0.3-2.3-2.1c-0.1-0.5-0.3-1-0.2-1.4 s0.4-0.9,0.8-1.2H0v8.7h52.3c3.3-2.9,7.5-4.6,12-4.5c0,1.3,0.6,2.1,1.7,2.2c1.4,0.1,2.9-0.4,4.2,0.4c0.6,0.6,1.3,1.3,1.9,1.9h4.2 c0,0,0,0,0.1-0.1c0.7,0,1.5,0,2.1,0c0,0,0,0,0.1,0.1h13.7c0.3-0.1,0.6-0.1,0.9,0c0.2,0,0.3,0,0.5,0h1.8c0.4,0,0.9,0,1.3,0h252 c0,0,0,0,0-0.1c0.2-0.4,0.6-1.1,0.7-1.1c0.8,0.3,1.3,0.7,1.4,1.2h164.9c0.1-2.6,1-3.2,4-3c0.4,0,0.8,0.4,1.1,0.8 c0.1,0.2-0.1,0.7-0.3,1c-0.2,0.4-0.5,0.8-0.9,0.9c-0.5,0.1-1,0.2-1.4,0.3h34c0.3-0.2,0.6-0.3,1-0.2c0.1,0.1,0.2,0.1,0.3,0.2h13.7 c0.1-0.2,0.3-0.4,0.5-0.4c0.9-0.3,1.8-0.5,2.7-0.5l0.7,0.9h240.2c0.3-1.1,0.2-2.1-0.5-3.1c-0.8-1.1-0.8-1.7,0-2.2 c0.9-0.5,1.7-0.4,2.7,0.1c1.8,0.9,2.2,1.5,1.6,3.3c-0.2,0.7-0.4,1.3-0.7,1.9H849V0.2H601.7C601.8,0.9,601.6,1.4,601,1.8z M343.9,8.7 c-0.1,0.1-0.7-0.5-1.1-0.7c-0.4-0.6-0.3-1.3,0.6-2c0.1-0.1,0.8,0.4,1.1,0.7C345,7.2,344.8,8,343.9,8.7z'/%3E%3C/svg%3E");
+//   background-position: center top;
+//   background-repeat: repeat;
+//   background-size: 849px 9px;
+//   border: none;
+//   border-radius: 4px;
+//   clear: both;
+//   display: block;
+//   height: 9px;
+//   outline: none;
+//   padding: 0;
+//   width: auto;
+// }
+//
+// .container {
+//   max-width: 960px;
+//   margin: 0 auto;
+// }
+//
+// .row {
+//   margin: 4em 0;
+// }
+//
+// header {
+//   text-align: center;
+// }
+//
+// .logo {
+//   height: 10em;
+// }
+//
+// footer {
+//   text-align: center;
+//   text-transform: uppercase;
+// }
---
+++ components/StyledText.js
@@ -0,0 +1,6 @@
+import * as React from 'react';
+import { Text } from 'react-native';
+
+export function CourierText(props) {
+  return <Text {...props} style={[props.style, { fontFamily: 'courier-prime' }]} />;
+}
