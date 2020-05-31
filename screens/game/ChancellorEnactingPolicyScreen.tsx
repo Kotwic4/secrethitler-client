2ebb0ab2fe7fb2f6e891e5046c4203484af80fac
Dariusz Szymczyk <edarek@op.pl>
2020-05-31 10:15:58 +0200

    Chancellor enacting policy screen styled.

--- screens/game/ChancellorEnactingPolicyScreen.tsx
+++ screens/game/ChancellorEnactingPolicyScreen.tsx
@@ -1,46 +1,49 @@
 import React, {useState} from 'react';
-import {StyleSheet, Text} from 'react-native';
+import {StyleSheet, View} from 'react-native';
 import {SmallLayout} from "../Layout";
-import {CourierText} from "../../components/CourierText";
 import {filterUsers} from '../../utils/filterUsers';
+import {Title} from "../../components/Title";
+import {CardBox} from "../../components/CardBox";
 
 export function ChancellorEnactingPolicyScreen({state, sendCommand, userName}) {
     const [top2Cards, setTop2Cards] = useState(state.game.policy_stack.slice(0, 2));
+    const [loading, setLoading] = useState(false);
     const pickCards = (withoutCardIndex) => {
-        const pickedCards = [...top2Cards];
-        pickedCards.splice(withoutCardIndex, 1);
+        const pickedCard = top2Cards.slice(withoutCardIndex, withoutCardIndex+1)[0];
+        setLoading(true);
 
         sendCommand({
             action: "chancellor_enacted_policy",
-            enacted: pickedCards
-        })
+            enacted: pickedCard
+        });
     };
     const cards = top2Cards.map((card, index) => {
-        return <Text style={styles.card} onPress={() => pickCards(index)}>
-            {card}
-        </Text>
+        return <CardBox card={card} disabled={loading} onPress={() => pickCards(index)}/>
     });
 
     if (state.game.chancellor !== userName) {
         return (
             <SmallLayout>
-                <CourierText>Waiting for chancellor({state.game.chancellor}) to pick one policiy</CourierText>
+                <Title>Waiting for chancellor ({state.game.chancellor}) to enact policy...</Title>
             </SmallLayout>
         );
     } else {
         return (
             <SmallLayout>
-                <CourierText>ChancellorEnactingPolicyScreen</CourierText>
-                {cards}
+                <Title>Enact policy</Title>
+                <View style={styles.cardsContainer}>
+                    {cards}
+                </View>
             </SmallLayout>
         );
     }
 }
 
 const styles = StyleSheet.create({
-    card: {
-        backgroundColor: "yellow",
-        marginBottom: 10,
-        padding: 10
+    cardsContainer: {
+        display: "flex",
+        flexDirection: "row",
+        flexWrap: "wrap",
+        justifyContent: "space-around"
     }
 });
